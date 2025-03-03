0006841fb11ace6a2129bbf243acaa4809ba7f98
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 02:00:38 +0530

    Update Script-12.sql

--- Script-12.sql
+++ Script-12.sql
@@ -16,3 +16,6 @@ where f.FACILITY_TYPE_ID = 'CONFIGURATION' and pf.MINIMUM_STOCK is not null ;
 
 --EXPLANATION (APPROACH)
 --- A facility whose facility type is CONFIGURATION and parent facility type is virtual then in such scenarios the minimum_stock is considered as threshold. so here we start with productFacility table and selects the facility whose type in configuration
+
+
+-- COST : 39529.12
