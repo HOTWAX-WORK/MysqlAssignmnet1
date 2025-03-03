e556313a551a3611cda4ae09babd73c3369602ec
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 01:54:09 +0530

    Update Script-5.sql

--- Script-5.sql
+++ Script-5.sql
@@ -1,3 +1,23 @@
+-- Completed Orders in August 2023
+-- Business Problem:
+-- After running similar reports for a previous month, you now need all completed orders in August 2023 for analysis.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- PRODUCT_TYPE_ID
+-- PRODUCT_STORE_ID
+-- TOTAL_QUANTITY
+-- INTERNAL_NAME
+-- FACILITY_ID
+-- EXTERNAL_ID
+-- FACILITY_TYPE_ID
+-- ORDER_HISTORY_ID
+-- ORDER_ID
+-- ORDER_ITEM_SEQ_ID
+-- SHIP_GROUP_SEQ_ID
+
+
 select  p.PRODUCT_ID  , p.PRODUCT_TYPE_ID ,
 oh.PRODUCT_STORE_ID ,oi.QUANTITY 
 ,p.INTERNAL_NAME ,f.FACILITY_ID ,
