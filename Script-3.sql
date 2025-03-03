3ec5bd5a93ca35d6424b366772f756c4cdd7372a
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 02:01:43 +0530

    Update Script-3.sql

--- Script-3.sql
+++ Script-3.sql
@@ -17,3 +17,5 @@ where gi.GOOD_IDENTIFICATION_TYPE_ID='ERP_ID' and gi.ID_VALUE is null;
 
 --EXPLANATION (APPROACH)
 ---It selects the PRODUCT_ID, INTERNAL_NAME, PRODUCT_TYPE_ID from the product table and the GOOD_IDENTIFICATION_TYPE_ID from the good_identification table where the GOOD_IDENTIFICATION_TYPE_ID is 'ERP_ID' (representing the NetSuite ID) and the ID_VALUE is null, indicating that the product either lacks or has an invalid NetSuite ID and needs to be updated in NetSuite.
+
+-- COST : 2.19
